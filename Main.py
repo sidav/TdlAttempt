47954126033f85d6d25cafdad37a09b20b3c3e11
VKovun <sidav94>
2017-04-25 16:54:42 +0300

    shit

--- Main.py
+++ Main.py
@@ -4,11 +4,12 @@ from SidavRandom import *
 
 playerx = SCREEN_WIDTH // 2
 playery = SCREEN_HEIGHT // 2
+exit_game = False
 
 def keys():
-    global playerx, playery
+    global playerx, playery, exit_game
     keypressed = readKey()
-    if (keypressed.key == 'ESCAPE') or (keypressed.key == 'ESCAPE'): return True
+    if (keypressed.key == 'ESCAPE') or (keypressed.key == 'ESCAPE'): exit_game = True
     if (keypressed.key == 'UP') or (keypressed.key == 'KP8'): playery -= 1
     if (keypressed.key == 'DOWN'): playery += 1
     if (keypressed.key == 'LEFT'): playerx -= 1
@@ -20,15 +21,15 @@ i = 127
 while not tdl.event.is_window_closed():
     setForegroundColor(rand(256), rand(256), rand(256))
     string = "Look at my fucking console. Look. "
-    for y in range(SCREEN_HEIGHT):
-        for x in range (SCREEN_WIDTH):
-            setForegroundColor(rand(256), rand(256), rand(256))
-            putChar(string[(x+y)%len(string)], x, y)
-            #tdl.flush()
+    # for y in range(SCREEN_HEIGHT):
+    #     for x in range (SCREEN_WIDTH):
+    #         setForegroundColor(rand(256), rand(256), rand(256))
+    #         putChar(string[(x+y)%len(string)], x, y)
+    #         #tdl.flush()
     putChar("@", playerx, playery)
     tdl.flush()
-    putChar(" ", playerx, playery)
+    # putChar(" ", playerx, playery)
     # handle keys and exit game if needed
-    exit_game = keys()
+    keys()
     if exit_game:
         break
--- Pathfinding.py
+++ Pathfinding.py
@@ -1,5 +1,11 @@
-#Here I'll try to implement the A* pathfinding.
+import numpy
+
+  #Here I'll try to implement the A* pathfinding.
+
 class pathfinder:
-    
-    def __init__(self):
-        pass
+
+    boolMap = numpy.zeros((80, 25))  #[[[False for x in range(80)] for y in range(25)]]
+
+    def __init__(self, inpBoolMap):
+        global boolMap
+        boolMap = inpBoolMap
