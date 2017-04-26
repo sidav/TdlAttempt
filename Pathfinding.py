b364641b27a2c91aa7c2150f9db6dc96a831cd4e
VKovun <sidav94>
2017-04-26 17:57:37 +0300

    A* suffering

--- Pathfinding.py
+++ Pathfinding.py
@@ -1,11 +1,44 @@
 import numpy
+import math
 
   #Here I'll try to implement the A* pathfinding.
 
-class pathfinder:
 
-    boolMap = numpy.zeros((80, 25))  #[[[False for x in range(80)] for y in range(25)]]
+class Pathfinder:
+    ######################################
+    class Cell:
+        x = y = g = 0
+        def __init__(self, xcor, ycor, g_):
+            global x, y, g
+            x = xcor
+            y = ycor
+            g = g_
+    #######################################
+
+    STRAIGHT_COST = 10
+    DIAGONAL_COST = 14
+    boolMap = []
+    openlist = [] #list of cells which we have to check
+    closedlist = [] #list of checked cells
+
+    def pathfindStart(self, fromx, fromy, tox, toy):
+        global openlist, closedlist
+        startnode = self.Cell(fromx, fromy, 0)
+        openlist.append(startnode)
+        closedlist.append(startnode)
+        self.checkNeighbours()
+        openlist.remove(startnode)
+
+
+    def calc_H(self, fromx, fromy, tox, toy):
+        return 10*abs(tox-fromx + toy-fromy)
+
+    def checkNeighbours(self, curcell):
+        x = curcell.x
+        y = curcell.y
+        pass
+
 
     def __init__(self, inpBoolMap):
         global boolMap
-        boolMap = inpBoolMap
+        pass
