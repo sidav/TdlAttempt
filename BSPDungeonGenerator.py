6a4a5a828fd34090b92a88a1aa48922160e9ab0f
Vladislav Kovun <vkovun@akb-it.ru>
2017-07-19 15:46:31 +0300

    WIP: Making the BSP generator convenient for independent use.

--- BSPDungeonGenerator.py
+++ BSPDungeonGenerator.py
@@ -5,6 +5,17 @@ from ConsoleWrapper import drawCharArray, setForegroundColor, putChar
 def random(min, max): #IT'S JUST A WRAPPER. Min, max inclusive!
     return rand(max-min+1)+min
 
+_MAP_WIDTH = 160
+_MAP_HEIGHT = 75
+_MIN_SPLIT_FACTOR = 25 #IT will be divided by 100 somewhen
+_MAX_SPLIT_FACTOR = 75 #It too
+_MIN_ROOM_WIDTH = 1
+_MIN_ROOM_HEIGHT = 1
+_SPLITS = 15
+
+_FLOOR_CODE = ' '
+_WALL_CODE = '#'
+_DOOR_CODE = '+'
 
 class treeNode:
     def __init__(self, parent=None, cont=None):
@@ -45,15 +56,15 @@ class treeNode:
         for _ in range(5): #5 is just a number of tries
             horizOK = True
             vertOK = True
-            factor = random(MIN_SPLIT_FACTOR, MAX_SPLIT_FACTOR)
+            factor = random(_MIN_SPLIT_FACTOR, _MAX_SPLIT_FACTOR)
             lefthorizh = selfh*factor//100
             righthorizh = selfh - lefthorizh
             leftvertw = selfw*factor//100
             rightvertw = selfw - leftvertw
-            if (lefthorizh < MIN_ROOM_HEIGHT or righthorizh < MIN_ROOM_HEIGHT):
+            if (lefthorizh < _MIN_ROOM_HEIGHT or righthorizh < _MIN_ROOM_HEIGHT):
                 horiz = 0
                 horizOK = False
-            if (leftvertw < MIN_ROOM_WIDTH or rightvertw < MIN_ROOM_WIDTH):
+            if (leftvertw < _MIN_ROOM_WIDTH or rightvertw < _MIN_ROOM_WIDTH):
                 vertOK = False
                 continue
         if not (horizOK and vertOK):
@@ -84,17 +95,11 @@ class Container:
         h = self.h+1
         w = self.w+1
         for i in range(x0, x0 + w):
-            arr[i][y0] = "#"
-            arr[i][y0+h-1] = "#"
+            arr[i][y0] = _WALL_CODE
+            arr[i][y0+h-1] = _WALL_CODE
         for j in range(y0, y0 + h):
-            arr[x0][j] = "#"
-            arr[x0+w-1][j] = "#"
-
-    # def draw(self): #for debug purposes
-    #     drawRect(self.x-1,self.y-1,self.w+1,self.h+1)
-
-    def output(self): #for debug purposes
-        print("x = %i, y = %i, w = %i, h = %i, splitting was %s" % (self.x,self.y,self.w,self.h, self.vh))
+            arr[x0][j] = _WALL_CODE
+            arr[x0+w-1][j] = _WALL_CODE
 #############################################################################################################
 #############################################################################################################
 def splitNTimes(root, N):
@@ -121,57 +126,46 @@ def placeConnections(root, arr):
                 ty = i.right.cont.y + i.right.cont.h // 2
                 if fx == tx:
                     for k in range(fy, ty + 1):
-                        arr[fx][k] = " "
+                        arr[fx][k] = _FLOOR_CODE
                 elif fy == ty:
                     for k in range(fx, tx + 1):
-                        arr[k][fy] = " "
+                        arr[k][fy] = _FLOOR_CODE
         curlvl += 1
 
 def placeDoors(arr):
     for i in range(1, len(arr)-1):
         for j in range(1, len(arr[0])-1):
             #horizontal doors:
-            if arr[i][j] == " " and arr[i][j-1] == "#" and arr[i][j+1] == "#" and arr[i-1][j] == " " and arr[i+1][j] == " " \
-                    and (arr[i-1][j - 1] == " " or arr[i+1][j - 1] == " " or arr[i-1][j + 1] == " " or arr[i+1][j + 1] == " "  ):
-                arr[i][j] = "+"
+            if arr[i][j] == _FLOOR_CODE and arr[i][j-1] == _WALL_CODE and arr[i][j+1] == _WALL_CODE and arr[i-1][j] == _FLOOR_CODE and arr[i+1][j] == _FLOOR_CODE \
+                    and (arr[i-1][j - 1] == _FLOOR_CODE or arr[i+1][j - 1] == _FLOOR_CODE or arr[i-1][j + 1] == _FLOOR_CODE or arr[i+1][j + 1] == _FLOOR_CODE  ):
+                arr[i][j] = _DOOR_CODE
             #vertical doors:
-            elif arr[i][j] == " " and arr[i-1][j] == "#" and arr[i+1][j] == "#" and arr[i][j-1] == " " and arr[i][j+1] == " " \
-                    and (arr[i - 1][j - 1] == " " or arr[i + 1][j - 1] == " " or arr[i - 1][j + 1] == " " or arr[i + 1][j + 1] == " "):
-                arr[i][j] = "+"
+            elif arr[i][j] == _FLOOR_CODE and arr[i-1][j] == _WALL_CODE and arr[i+1][j] == _WALL_CODE and arr[i][j-1] == _FLOOR_CODE and arr[i][j+1] == _FLOOR_CODE \
+                    and (arr[i - 1][j - 1] == _FLOOR_CODE or arr[i + 1][j - 1] == _FLOOR_CODE or arr[i - 1][j + 1] == _FLOOR_CODE or arr[i + 1][j + 1] == _FLOOR_CODE):
+                arr[i][j] = _DOOR_CODE
 
+
+def makeWallOutline(arr): #just to be sure that the map won't be open to its borders.
+    for i in range(len(arr)):
+        for j in range(len(arr[0])):
+            if i == 0 or j == 0 or i == len(arr)-2 or j == len(arr[0])-2:
+                arr[i][j] = _WALL_CODE
 
 
 def generateMap():
-    outp = [[" "] * (MAP_HEIGHT+1) for _ in range(MAP_WIDTH+1)]
-    con = Container(1,1,MAP_WIDTH-1,MAP_HEIGHT-1)
+    outp = [[_FLOOR_CODE] * (_MAP_HEIGHT+1) for _ in range(_MAP_WIDTH+1)]
+    con = Container(1,1,_MAP_WIDTH-1,_MAP_HEIGHT-1)
     BSPRoot = treeNode(cont = con)
-    splitNTimes(BSPRoot, SPLITS)
+    splitNTimes(BSPRoot, _SPLITS)
     leafs = BSPRoot.getLeafs()
-    for i in leafs:#leafs:
-        # i.cont.output()
-        # i.cont.draw()
+    for i in leafs:
         i.cont.addToMap(outp)
     placeConnections(BSPRoot, outp)
     placeDoors(outp)
+    makeWallOutline(outp)
     #DELETE...
     # draw the char array (it is just for debug)
     setForegroundColor(255, 255, 255)
     drawCharArray(outp)
-    #...UNTIL THERE
+    #...UNTIL THERE.
     return outp
-
-
-
-MAP_WIDTH = 160
-MAP_HEIGHT = 75
-MIN_SPLIT_FACTOR = 25 #IT will be divided by 100 somewhen
-MAX_SPLIT_FACTOR = 75 #It too
-MIN_ROOM_WIDTH = 2
-MIN_ROOM_HEIGHT = 2
-SPLITS = 15
-
-# FLOOR = 0
-# WALL = 1
-# DOOR = 2
-
-
